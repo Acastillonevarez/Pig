58bba3506907ba68d52cd2825b4965a240b29474
Andrew <acastillonevarez@bsd220.org>
2022-11-16 09:22:08 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Andrew Castillo Nevarez on 11/16/22.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
